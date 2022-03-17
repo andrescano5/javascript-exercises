2c93354f89438d61c3823d519c935f71e81fb3d1
andrescano5 <andrescano7.ac@gmail.com>
2022-03-16 21:03:25 -0300

    From hello world up to tempConversions exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    word = ''
+    if(num === 0 || num < 0) return false
+    for(let i = 0;i < num; i++) {
+        word += string
+    }
+    return word
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const ftoc = function() {
+celsius = 0;
+fahrenheit = 32
+
+const ftoc = function(far, cel) {
+   celResult = cel === (far - 32) / 1.8
+   result = Math.round(celResult * 10) /10
+   return result
 
 };
 
-const ctof = function() {
-
+const ctof = function(far, cel) {
+  far = fahrenheit;
+  cel = celsius;
+  farResult = far === (cel * 1.8) + 32
+  result = Math.round(farResult * 10) /10
+  return result
+   
 };
 
 // Do not edit below this line
